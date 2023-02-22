682812852e5dccb2c71ba10d2e3b9fd43443f5ab
김동우 <66302122+DongWooKim97@users.noreply.github.com>
2023-02-22 09:11:10 +0900

    fix: 메인 메소드 문구 수정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("2월 22일입니다.");
     }
 }
